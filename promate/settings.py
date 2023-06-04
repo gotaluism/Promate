75ff32158c81a08ae6f926bb43f16ab56b3f8154
mvasqueze <68928428+mvasqueze@users.noreply.github.com>
2023-06-04 18:35:54 -0500

    Update settings.py

--- promate/settings.py
+++ promate/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%c%m!@4rdinf)o+n6z*tvnveeo27@*_g+l*osx%(dwn--mqa8e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.208.136.1']
+ALLOWED_HOSTS = ['35.208.136.1', '127.0.0.1']
 
 
 # Application definition
